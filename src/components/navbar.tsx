9169dafae014cf206b6ec6282890281f88a4a64a
28n <28nh04@gmail.com>
2022-11-05 12:35:01 +0100

    bootstrap

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import { NextComponentType } from "next";
+
+const Navbar: NextComponentType = () => {
+  return (
+    <>
+      Navbar
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      Home
+    </>
+  );
+};
+
+export default Home;
